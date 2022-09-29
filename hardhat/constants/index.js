749d350ffaebdb82f38d0f51deb8dfaeeaaa8bc6
ggomes.terceiro <ggomes.terceiro>
2022-09-29 15:23:57 -0300

    Setup of hardhat project

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x8Fde1EDA759512b58128Cec5B05168b49E9Fec66";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
